9c8250bdc183fe8bf2093319b8064f4982b2a016
Yves Sucaet <37899889+YvesSucaet@users.noreply.github.com>
2019-04-17 19:15:04 +0200

    Introducing new AdminConnect() and AdminDisconnect() methods in the CoreAdmin class

--- src/lib_pathomation.php
+++ src/lib_pathomation.php
@@ -12,12 +12,12 @@ Class that wraps around the free PMA.core.lite (the server component of PMA.star
 */
 class Core {
 	# internal module helper variables and functions
-	private static $_pma_sessions = [];
-	private static $_pma_slideinfos = [];
+	public static $_pma_sessions = [];
+	public static $_pma_slideinfos = [];
 	public static $_pma_pmacoreliteURL = "http://localhost:54001/";
 	public static $_pma_pmacoreliteSessionID = "SDK.PHP";
 	private static $_pma_usecachewhenretrievingtiles = true;
-	private static $_pma_amount_of_data_downloaded = array("SDK.PHP" => 0);
+	public static $_pma_amount_of_data_downloaded = array("SDK.PHP" => 0);
 
 	/** Internal use only */
 	private static function _pma_session_id($sessionID = null)
@@ -899,24 +899,78 @@ class UI {
 CoreAdmin class. Interface to PMA.core for administrative operations. Does NOT apply to PMA.start / PMA.core.lite
 */
 class CoreAdmin {
+
+	/**
+    Attempt to connect to PMA.core instance; success results in a SessionID
+    only success if the user has administrative status
+    */
+	public static function AdminConnect($pmacoreURL, $pmacoreAdmUsername, $pmacoreAdmPassword){ 
+		if ($pmacoreURL == Core::$_pma_pmacoreliteURL) {
+			if (Core::is_lite()) {
+				throw new \BadMethodCallException("PMA.core.lite found running, but doesn't support an administrative back-end");
+			} else {
+				throw new \BadMethodCallException("PMA.core.lite not found, and besides; it doesn't support an administrative back-end anyway");
+			}
+		}
+		// purposefully DON'T use helper function _pma_admin_url() here:    
+		// why? Because_pma_admin_url() takes session information into account (which we don't have yet)
+		$url = PMA::_pma_join($pmacoreURL, "admin/json/AdminAuthenticate?caller=SDK.PHP");
+		$url .= "&username=".PMA::_pma_q($pmacoreAdmUsername);
+		$url .= "&password=".PMA::_pma_q($pmacoreAdmPassword);
+
+		try {
+			$contents = @file_get_contents($url);
+		} catch (Exception $ex) {
+			throw new Exception("Unable to login $pmacoreAdmUsername on $pmacoreURL");
+			$contents = "";
+		}
+
+		$json = json_decode($contents, true);
+		if (isset($json["d"])) {
+			$json = $json["d"];
+		}
+
+		$loginresult = $json;
+		
+		if ($loginresult["Success"] != 1) {
+			$admSessionID = null;
+		} else {
+			$admSessionID = $loginresult["SessionId"];
+			
+			Core::$_pma_sessions[$admSessionID] = $pmacoreURL;
+			if (!isset(Core::$_pma_slideinfos[$admSessionID])) {
+				Core::$_pma_slideinfos[$admSessionID] = array();
+			}
+			Core::$_pma_amount_of_data_downloaded[$admSessionID] = strlen($contents);
+		}
+		
+		return ($admSessionID);
+	}
+
+	/**
+	Attempt to disconnect from PMA.core instance; True if valid admSessionID was indeed disconnected
+	*/
+	public static function AdminDisconnect($admSessionID) {
+		return Core::disconnect($admSessionID);
+	}
 	
 	/**
 	Define a new user in PMA.core
 	Returns true if user creation is successful; false if not.
 	*/
-	public static function AddUser($ASessionID, $login, $firstName, $lastName, $email, $password, $canAnnotate = false, $isAdmin = false, $isSuspended = false) {
-		if (Core::$_pma_pmacoreliteSessionID == $ASessionID) {
+	public static function AddUser($AdmSessionID, $login, $firstName, $lastName, $email, $password, $canAnnotate = false, $isAdmin = false, $isSuspended = false) {
+		if (Core::$_pma_pmacoreliteSessionID == $AdmSessionID) {
 			throw new \BadMethodCallException("PMA.start doesn't support AddUser()");
 		}
 		
-		$url = Core::_pma_url($ASessionID)."admin?singleWsdl";
+		$url = Core::_pma_url($AdmSessionID)."admin?singleWsdl";
 		print($url);
 		$client = new \SoapClient($url);
 
 		try {	
 			$client->CreateUser(
 				array(
-					"sessionID" => $ASessionID,
+					"sessionID" => $AdmSessionID,
 					"user" => array(
 							"Administrator" => $isAdmin,
 							"CanAnnotate"   => $canAnnotate,
@@ -936,12 +990,12 @@ class CoreAdmin {
 		}
 		return true;
 	}
-	public static function UserExists($ASessionID, $login) {
-		if (Core::$_pma_pmacoreliteSessionID == $ASessionID) {
+	public static function UserExists($AdmSessionID, $login) {
+		if (Core::$_pma_pmacoreliteSessionID == $AdmSessionID) {
 			throw new \BadMethodCallException("PMA.start doesn't support UserExists()");
 		}
 
-		$url = Core::_pma_url($ASessionID)."admin/json/SearchUsers?sessionID=".PMA::_pma_q($ASessionID)."&source=local&query=".PMA::_pma_q($login);
+		$url = Core::_pma_url($AdmSessionID)."admin/json/SearchUsers?sessionID=".PMA::_pma_q($AdmSessionID)."&source=local&query=".PMA::_pma_q($login);
 		try {
 			$contents = @file_get_contents($url);
 		} catch (Exception $ex) {
@@ -956,15 +1010,15 @@ class CoreAdmin {
 		return count($json) > 0;
 	}
 	
-	public static function AddS3RootDirectory($ASessionID, $s3accessKey, $s3secretKey, $alias, $s3path, $description = "Root dir created through lib_php", $isPublic = False, $isOffline = False) {
-		if (Core::$_pma_pmacoreliteSessionID == $ASessionID) {
+	public static function AddS3RootDirectory($AdmSessionID, $s3accessKey, $s3secretKey, $alias, $s3path, $description = "Root dir created through lib_php", $isPublic = False, $isOffline = False) {
+		if (Core::$_pma_pmacoreliteSessionID == $AdmSessionID) {
 			throw new \BadMethodCallException("PMA.start doesn't support AddS3RootDirectory()");
 		}
 		
-		$url = Core::_pma_url($ASessionID)."admin/json/CreateAmazonS3RootDirectory";
+		$url = Core::_pma_url($AdmSessionID)."admin/json/CreateAmazonS3RootDirectory";
 
 		$jsonData = array(
-		 "sessionID" => $ASessionID,
+		 "sessionID" => $AdmSessionID,
 		  "rootDirectory"=> array(
 			"AccessKey"=> $s3accessKey,
 			"SecretKey"=> $s3secretKey,
@@ -980,15 +1034,15 @@ class CoreAdmin {
 		return $ret_val;
 	}
 
-	public static function AddLocalRootDirectory($ASessionID, $alias, $localpath, $description = "Root dir created through lib_php", $isPublic = False, $isOffline = False) {
-		if (Core::$_pma_pmacoreliteSessionID == $ASessionID) {
+	public static function AddLocalRootDirectory($AdmSessionID, $alias, $localpath, $description = "Root dir created through lib_php", $isPublic = False, $isOffline = False) {
+		if (Core::$_pma_pmacoreliteSessionID == $AdmSessionID) {
 			throw new \BadMethodCallException("PMA.start doesn't support AddLocalRootDirectory()");
 		}
 		
-		$url = Core::_pma_url($ASessionID)."admin/json/CreateFileSystemRootDirectory";
+		$url = Core::_pma_url($AdmSessionID)."admin/json/CreateFileSystemRootDirectory";
 
 		$jsonData = array(
-		 "sessionID" => $ASessionID,
+		 "sessionID" => $AdmSessionID,
 		  "rootDirectory"=> array(
 			"Alias"=> $alias,
 			"Description"=> $description,
@@ -1002,16 +1056,16 @@ class CoreAdmin {
 		return $ret_val;
 	}
 	
-	public static function GrantAccessToRootDirectory($ASessionID, $pmacoreUsername, $alias) {
+	public static function GrantAccessToRootDirectory($AdmSessionID, $pmacoreUsername, $alias) {
 
-		if (Core::$_pma_pmacoreliteSessionID == $ASessionID) {
+		if (Core::$_pma_pmacoreliteSessionID == $AdmSessionID) {
 			throw new \BadMethodCallException("PMA.start doesn't support GrantAccessToRootDirectory()");
 		}
 		
-		$url = Core::_pma_url($ASessionID)."admin/json/GrantRootDirAccess";
+		$url = Core::_pma_url($AdmSessionID)."admin/json/GrantRootDirAccess";
 
 		$jsonData = array(
-		 "sessionID"=> $ASessionID,
+		 "sessionID"=> $AdmSessionID,
 		 "usernames"=> array($pmacoreUsername),
 		 "rootDirectories"=> array($alias)
 		);
@@ -1020,16 +1074,16 @@ class CoreAdmin {
 		return $ret_val;
 	}
 	
-	public static function DenyAccessToRootDirectory($ASessionID, $pmacoreUsername, $alias) {
+	public static function DenyAccessToRootDirectory($AdmSessionID, $pmacoreUsername, $alias) {
 
-		if (Core::$_pma_pmacoreliteSessionID == $ASessionID) {
+		if (Core::$_pma_pmacoreliteSessionID == $AdmSessionID) {
 			throw new \BadMethodCallException("PMA.start doesn't support DenyAccessToRootDirectory()");
 		}
 		
-		$url = Core::_pma_url($ASessionID)."admin/json/DenyRootDirAccess";
+		$url = Core::_pma_url($AdmSessionID)."admin/json/DenyRootDirAccess";
 
 		$jsonData = array(
-		 "sessionID"=> $ASessionID,
+		 "sessionID"=> $AdmSessionID,
 		 "usernames"=> array($pmacoreUsername),
 		 "rootDirectories"=> array($alias)
 		);
@@ -1171,8 +1225,8 @@ class Control {
 Helper class. Developers should never access this class directly (but may recognize some helper functions they wrote themselves once upon a time)
 */
 class PMA {
-	/** returns the current version of the library (2.0.0.30) */
-	const version = "2.0.0.30";
+	/** returns the current version of the library (2.0.0.32) */
+	const version = "2.0.0.32";
 
 	/** Internal use only */
 	public static function ends_with($wholestring, $suffix)
